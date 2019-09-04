6231fbe1a5884fe1ee734596cfc61971fcaadc4d
U-DESKTOP-IH4NTDI\Artur <Artur Yumaev>
2019-09-04 18:06:10 +0300

    Done with algorithms

--- lab1/main.py
+++ lab1/main.py
@@ -23,7 +23,17 @@ def levenshtein_matrix(s1, s2):
     return matrix, distance
 
 def levenshtein_rec(s1, s2):
-    pass
+    i, j = len(s1), len(s2)
+
+    if i == 0:
+        return j
+    if j == 0:
+        return i
+    if i == 0 and j == 0:
+        return 0
+    return min(levenshtein_rec(s1[0:i], s2[0:j - 1]) + 1,
+               levenshtein_rec(s1[0:i - 1], s2[0:j]) + 1,
+               levenshtein_rec(s1[0:i - 1], s2[0:j - 1]) + (0 if s1[i - 1] == s2[j - 1] else 1))
 
 def domerau_levenshtein_matrix(s1, s2):
     matrix = alloc_matrix(s1, s2)
@@ -54,16 +64,32 @@ def domerau_levenshtein_matrix(s1, s2):
     return matrix, distance
 
 def domerau_levenshtein_rec(s1, s2):
-    pass
+    i, j = len(s1), len(s2)
+
+    if i == 0:
+        return j
+    if j == 0:
+        return i
+    if i == 0 and j == 0:
+        return 0
+    return min(levenshtein_rec(s1[0:i], s2[0:j - 1]) + 1,
+               levenshtein_rec(s1[0:i - 1], s2[0:j]) + 1,
+               levenshtein_rec(s1[0:i - 1], s2[0:j - 1]) + (0 if s1[i - 2] == s2[j - 2] else 1),
+               levenshtein_rec(s1[0:i - 2], s2[0:j - 2]) + 1,
+               0 if ((s1[i - 2] == s2[j - 1]) and (s1[i - 1] == s2[j - 2])) else 1)
 
 def alloc_matrix(s1, s2):
     matrix_shape = (len(s1) + 1, len(s2) + 1)
     return np.zeros(matrix_shape)
 
-def print_matrix(title, s1, s2, matrix, distance):
-    print(title)
-    print(matrix)
-    print(distance, end="\n\n")
+def print_result(title, s1, s2, distance, matrix=None):
+    print("Distance between \"{0}\" and \"{1}\" according to {2} is {3}".format(s1, s2, title, int(distance)))
+    if matrix is not None:
+        print(matrix)
+    print()
+
+def measure_time():
+    pass
 
 def test():
     pass
@@ -72,13 +98,19 @@ def test_all():
     pass
 
 def main():
-    s1, s2 = "кот", "окт"
+    s1, s2 = "скат", "кот"
     
     lm, lm_d = levenshtein_matrix(s1, s2)
-    print_matrix("Levenshtein matrix", s1, s2, lm, lm_d)
+    print_result("Levenshtein Matrix", s1, s2, lm_d, matrix=lm)
 
     dlm, dlm_d = domerau_levenshtein_matrix(s1, s2)
-    print_matrix("Domerau-Levenshtein matrix", s1, s2, dlm, dlm_d)
+    print_result("Domerau-Levenshtein Matrix", s1, s2, dlm_d, matrix=dlm)
+
+    lrd = levenshtein_rec(s1, s2) # Levenstein recursion distance
+    print_result("Levenstain Recursive Method", s1, s2, lrd)
+
+    dlrd = domerau_levenshtein_rec(s1, s2)
+    print_result("Domerau-Levenstain Recursive Method", s1, s2, dlrd)
 
 if __name__ == "__main__":
     main()
