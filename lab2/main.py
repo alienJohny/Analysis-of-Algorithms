90e49b80ea7e249baca008f2be3fb96a53e0d799
U-DESKTOP-IH4NTDI\Artur <Artur Yumaev>
2019-10-23 21:05:47 +0300

    updated to winograd algorithm classic

--- lab2/main.py
+++ lab2/main.py
@@ -2,7 +2,16 @@ from classic import classic_matrix_mult
 from winograd import classic_winograd, optimized_winograd
 from matplotlib import pyplot as plt
 import time, sys # Internal libraries
-import numpy
+import numpy as np
+
+
+def generate_matrixes(down_size, up_size, step):
+    matrixes = []
+    for i in range(down_size, up_size + step, step):
+        matrixes.append(np.random.randint(-150, 150, size=(i, i)))
+
+    return np.array(matrixes)
+
 
 def measure_time(times, f, *args):
     """
@@ -13,19 +22,35 @@ def measure_time(times, f, *args):
     time_start = time.process_time()
     for i in range(times):
         f(*args)
-    time_end = time.process_time() - time_start)
+    time_end = time.process_time() - time_start
     avg_time = time_end / times
 
     return avg_time
 
-def plot_results():
-    pass
+
+def plot_results(x, title, xlabel, ylabel, legend, *args):
+    print(args)
+    
+    plt.title(title)
+    plt.grid(True)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    for arg in args:
+        print(args)
+        plt.plot(x, arg, "o--")
+
+    plt.legend(legend)
+    plt.show()
+
 
 def test():
-    pass
+    plot_results([1, 2, 3, 4], "", "", "", 
+
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
--- lab2/winograd.py
+++ lab2/winograd.py
@@ -1,5 +1,50 @@
-def classic_winograd():
-    return 0
+import numpy as np
+
+def classic_winograd(A, B):
+    M = len(A)
+    N = len(B)
+    Q = len(B[0])
+    
+    C = np.zeros((M, Q)) # Will be result
+
+    if N != len(A[0]):
+        print("Different dimension of the matrics")
+        return None
+
+    MulH = np.zeros((M))
+    MulV = np.zeros((Q))
+
+    for i in range(M):
+        for j in range(N // 2):
+            MulH[i] += A[i][2 * j] * A[i][2 * j + 1]
+
+    for i in range(Q):
+        for j in range(N // 2):
+            MulV[i] += B[2 * j][i] * B[2 * j + 1][i]
+
+    for i in range(M):
+        for j in range(Q):
+            C[i][j] = - MulH[i] - MulV[j]
+            for k in range(N // 2):
+                C[i][j] += ((A[i][2 * k] + B[2 * k + 1][j]) * (A[i][2 * k + 1] + B[2 * k][j]))
+
+    if N % 2:
+        for i in range(M):
+            for j in range(Q):
+                C[i][j] += A[i][N - 1] * B[N - 1][j]
+
+    return np.array(C)
 
 def optimized_winograd():
     return 0
+
+if __name__ == "__main__":
+    A = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]
+    B = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7], [4, 5, 6, 7, 8]]
+    print(classic_winograd(A, B))
+
+    """
+    [[ 30.  40.  50.  60.  70.]
+     [ 40.  54.  68.  82.  96.]
+     [ 50.  68.  86. 104. 122.]]
+    """
